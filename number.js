ef9b5fdb363d6d93eab67440985b603c3e5b1448
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-10 22:37:59 +0600

    string

---
+++ number.js
@@ -0,0 +1,6 @@
+var n = 123
+var n2 = 123.123
+var n3 = 123e5
+var n4 = 123e-5
+
+console.log(n, n2, n3, n4)
---
+++ string.js
@@ -0,0 +1,8 @@
+var str = 'Shah Ehsan'
+var str2 = "Shah Ehsan"
+var str3 = `Shah Ehsan`
+var str4 = String('Shah Ehsan')
+var str5 = String(289)
+var str6 = String(4.753)
+
+console.log(str, str2, str3, str4, str5, str6)
