a84a3951a02cafa6b992e54a3d1e580901ead8c9
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-10 22:54:42 +0600

    type conversion

---
+++ typeConversion.js
@@ -0,0 +1,27 @@
+var str = '1000'
+var n = 10
+
+console.log(str + n)
+console.log(Number(str) + n)
+console.log(Number.parseInt(str) )
+console.log(Number.parseFloat(str))
+console.log(Boolean(Infinity));
+
+
+
+// Falsy values
+// 0, 
+// '',
+//  null,
+//   undefined,
+//    NaN,
+//     false
+
+
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(NaN))
+console.log(Boolean(false));
+
