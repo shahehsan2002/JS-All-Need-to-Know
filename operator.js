90b6b097532fe30246528ce177d89ad619857daf
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-11 22:36:14 +0600

    arithmetic  operator

---
+++ operator.js
@@ -0,0 +1,24 @@
+// arithmetic  operator
+// plus (+)  minus (-)  multiply (*)  divide (/)  modulus (%)  exponent (^)  floor division (//)  increment (++), decrement (--)
+
+var a = 10
+var b =3
+
+// addition 
+console.log(a+b);
+// subtraction
+console.log(a-b);
+// multiplication
+console.log(a*b);
+// division
+console.log(a/b);
+// modulus
+console.log(a%b);
+// pre increment
+console.log(++a);
+// post increment
+console.log(a++);
+// pre decrement
+console.log(--b);
+// post decrement
+console.log(b--);
