ec02c492a7c8364195d73ebbd7543fa5e7dfbe7e
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-05 14:18:04 +0600

    explore math functionality

---
+++ math.js
@@ -0,0 +1,20 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+var n = 4.389;
+
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+
+console.log(Math.max(3, 4, 5, 6, 7));
+console.log(Math.min(3, 4, 5, 6, 7));
+
+
+console.log(Math.pow(3, 4));
+console.log(Math.sqrt(9));
+console.log(Math.cbrt(27));
+
+console.log(Math.round(Math.random() * 100+1));
+
