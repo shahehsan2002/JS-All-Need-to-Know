29c67406bb07372f59251d5d75df7e38362e4b30
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-05 18:25:43 +0600

    explore else if  condition

---
+++ elseCondition.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("b is greater than a");
+}
+
+var n = 50;
+
+if (n % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
---
+++ elseIFCondition.js
@@ -0,0 +1,21 @@
+var a = 20;
+var b = 20;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console.log("b is greater than a");
+}else{
+  console.log("a and b are equal");
+}
+
+var n = 0;
+
+if (n  == 0) {
+  console.log("zero");
+}
+else if (n % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
