3f10efc2ddbc0ccebcccd689bae22a9fe2cc5fc9
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-05 16:30:16 +0600

    explore if condition

---
+++ ifCondition.js
@@ -0,0 +1,19 @@
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log("a is greater than b");
+}
+
+if (a < b) {
+  console.log("b is greater than a");
+}
+
+var n = 50;
+
+if (n % 2 == 0) {
+  console.log("even");
+}
+if (n % 2 != 0) {
+  console.log("odd");
+}
