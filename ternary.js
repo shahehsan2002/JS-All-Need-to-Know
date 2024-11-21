ad0b8578b3279844f12057ccd80cdbc46406fb01
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-21 18:28:01 +0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,15 @@
+var n = 11
+var str = ''
+
+if (n %2 === 0){
+    str = 'EVEN'
+} else {
+    str = 'ODD'
+}
+
+console.log(str)
+
+var str2 = n%2 === 0 ? 'EVEN' : 'ODD'
+console.log(str2)
+
+// condition ? true : false
